ab7a32a9430dce1aed47449a7c93783187d88bf2
Martina Vitovska <409920@mail.muni.cz>
2016-12-23 11:51:41 +0100

    Add simple main page

--- library-IS/config/routes.rb
+++ library-IS/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'main_page/index'
+  root 'main_page#index'
 end
