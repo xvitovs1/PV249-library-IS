5fcf36be0af43166d5124f50a4f291fedcb7858e
Martina Vitovska <409920@mail.muni.cz>
2017-01-07 18:10:05 +0100

    Add new resources to routes

--- library-IS/config/routes.rb
+++ library-IS/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-
+  resources :authors
+  resources :books
+  resources :publications
+  resources :publishers
   get 'main_page/index'
   root 'main_page#index'
 end
