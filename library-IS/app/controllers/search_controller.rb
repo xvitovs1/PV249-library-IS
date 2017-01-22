e5150046af12dcc11a8cca2dde66c26c8f512c5e
Martina Vitovska <409920@mail.muni.cz>
2017-01-22 11:32:37 +0100

    Implement simple search for books

--- library-IS/app/controllers/search_controller.rb
+++ library-IS/app/controllers/search_controller.rb
@@ -4,6 +4,13 @@ class SearchController < ApplicationController
   end
 
   def search_books
+    if !params['title'].empty?
+      @books = search_books_by_title(params['title'])
+    elsif !params['author'].empty?
+      @books = search_books_by_author(params['author'])
+    else
+      @books = search_books_by_author(params['isbn'])
+    end
   end
 
   def search_authors
@@ -12,5 +19,18 @@ class SearchController < ApplicationController
 
   def search_publishers
     @publishers = Publisher.where(["name like ?", '%' + params['name'] + '%'])
+  end
+
+  private
+  def search_books_by_title(title)
+    return Book.where(["title like ?", '%' + title + '%'])
+  end
+
+  def search_books_by_author(author)
+    return Book.joins(:author).where(['authors.name LIKE ?', '%' + author + '%'])
+  end
+
+  def search_books_by_isbn(isbn)
+    return Book.where(["isbn = ?", isbn])
   end
 end
