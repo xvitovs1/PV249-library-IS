7d9c69d457969c08d435ea964ecd578742262acf
Martina Vitovska <409920@mail.muni.cz>
2017-01-23 15:18:04 +0100

    Add test for book model

--- library-IS/test/models/book_test.rb
+++ library-IS/test/models/book_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @author = Author.new(name: "test author")
+    @book = Book.new(title: "Test book", description: "some description")
+    @book.author = @author
+  end
+
+  test "should be valid" do
+    assert @book.valid?
+  end
+
+  test "title should be present" do
+    @book.title = "     "
+    assert_not @book.valid?
+  end
+
+  test "author should be present" do
+    @book.author = nil
+    assert_not @book.valid?
+  end
 end
