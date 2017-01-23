e1e30be3847caca095c9ebd4c403c6a96434921e
Martina Vitovska <409920@mail.muni.cz>
2017-01-23 16:04:14 +0100

    Add available? method for publications

--- library-IS/app/models/publication.rb
+++ library-IS/app/models/publication.rb
@@ -2,4 +2,9 @@ class Publication < ApplicationRecord
   belongs_to :publisher
   belongs_to :book
   validates :ISBN, presence: { message: "can't be blank" }
+
+  def available?
+    borrows = Borrow.where(["publication_id = ? and return_date = ?", self.id, nil])
+    return borrows.empty?
+  end
 end
