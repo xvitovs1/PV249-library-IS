2da649e2fb68ebaddfda7961e41859d8ea997444
Martina Vitovska <409920@mail.muni.cz>
2017-01-23 15:28:49 +0100

    Add test for publisher model

--- library-IS/test/models/publisher_test.rb
+++ library-IS/test/models/publisher_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PublisherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @publisher = Publisher.new(name: "Test publisher")
+  end
+
+  test "should be valid" do
+    assert @publisher.valid?
+  end
+
+  test "name should be present" do
+    @publisher.name = "     "
+    assert_not @publisher.valid?
+  end
 end
