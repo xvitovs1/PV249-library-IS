da7afa7c034dcb1aa3c4ac512f1977bd3c9ce624
Martina Vitovska <409920@mail.muni.cz>
2017-01-27 13:59:18 +0100

    Add tests for borrow model

--- library-IS/app/validators/borrow_validator.rb
+++ library-IS/app/validators/borrow_validator.rb
@@ -1,6 +1,7 @@
 class BorrowValidator < ActiveModel::Validator
   def validate(record)
-    publication = Publication.find(record.publication_id)
+    publication = Publication.find_by id: record.publication_id
+    return false unless publication
     return publication.available?
   end
 end
--- library-IS/test/models/borrow_test.rb
+++ library-IS/test/models/borrow_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class BorrowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    publication = publications(:one)
+    publication.save
+    @borrow = Borrow.new(borrow_date: Date.today, expected_return_date: Date.tomorrow, publication: publications(:one), reader: readers(:one))
+  end
+
+  test "should be valid" do
+    assert @borrow.valid?
+  end
+
+  test "publication should be present" do
+    @borrow.publication = nil
+    assert_not @borrow.valid?
+  end
+
+  test "reader should be present" do
+    @borrow.reader = nil
+    assert_not @borrow.valid?
+  end
+
+  test "should be active" do
+    assert @borrow.active?
+  end
+
+  test "should not be active" do
+    @borrow.return_date = Date.today
+    assert_not @borrow.active?
+  end
 end
