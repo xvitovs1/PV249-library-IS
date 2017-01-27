9bba1a856b8942c6ac33a17f804319e28fd9cb69
Martina Vitovska <409920@mail.muni.cz>
2017-01-27 13:36:06 +0100

    Add tests for models reader and librarian

--- library-IS/test/models/reader_test.rb
+++ library-IS/test/models/reader_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ReaderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+    @user = User.new(name: "Test user", email: "test@example.com", password: "pass", password_confirmation: "pass")
+    @reader = Reader.new(card_code: "123abc")
+    @user.reader = @reader
+  end
+
+  test "should be valid" do
+    assert @reader.valid?
+  end
+
+  test "card_code should be present" do
+    @reader.card_code = "     "
+    assert_not @reader.valid?
+  end
 end
