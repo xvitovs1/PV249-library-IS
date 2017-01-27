494c5a5ae126f7d37d0e978a0dfba69dfb9cbc31
Martina Vitovska <409920@mail.muni.cz>
2017-01-27 12:44:38 +0100

    Add validation for isbn uniqueness

--- library-IS/app/models/publication.rb
+++ library-IS/app/models/publication.rb
@@ -1,7 +1,7 @@
 class Publication < ApplicationRecord
   belongs_to :publisher
   belongs_to :book
-  validates :ISBN, presence: { message: "can't be blank" }
+  validates :ISBN, presence: { message: "can't be blank" }, uniqueness: {message: "must be unique"}
 
   def available?
     borrows = Borrow.where(["publication_id = ? and return_date IS NULL", self.id])
