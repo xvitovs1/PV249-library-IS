1a4f8bcf319bea2b47649137502bc900ac3d962d
Martina Vitovska <409920@mail.muni.cz>
2017-01-27 14:28:37 +0100

    Add some tests for authors controller

--- library-IS/test/controllers/authors_controller_test.rb
+++ library-IS/test/controllers/authors_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
 class AuthorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+  def setup
+    @author = authors(:one)
+    @author.save
+  end
+
+  test "should not get new when not authorized" do
     get authors_new_url
+    assert_redirected_to '/librarians/denied'
+  end
+
+  test "should not get edit when not authorized" do
+    get '/authors/:id/edit', params: {id: @author.id}
+    assert_redirected_to '/librarians/denied'
+  end
+
+  test "should get show" do
+    get "/authors/", params: {id: @author.id}
     assert_response :success
   end
 
+  test "should not delete when not authorized" do
+    delete "/authors/:id", params: {id: @author.id}
+    assert_redirected_to '/librarians/denied'
+  end
 end
